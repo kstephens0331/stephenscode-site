a6af70ce81d039615d3add8d5de72a62cd6066c2
kstephens0331 <Kyle_stephens31@icloud.com>
2025-05-21 05:23:26 -0500

    clean up /checkout listening port

--- clean-checkout-server/server.js
+++ clean-checkout-server/server.js
@@ -47,5 +47,8 @@ app.get("/", (req, res) => {
 });
 
 // ✅ Start Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+const PORT = 443;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
