366661661f5777b679ca09f08256f0f52d214f03
kstephens0331 <Kyle_stephens31@icloud.com>
2025-11-18 22:24:58 -0600

    Simplify next.config.ts

--- stephenscode-marketing/next.config.ts
+++ stephenscode-marketing/next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from 'next'
-import path from 'path'
 
-const nextConfig: NextConfig = {
-  // Fix multiple lockfiles warning
-  outputFileTracingRoot: path.join(__dirname, '../'),
-}
+const nextConfig: NextConfig = {}
 
 export default nextConfig
